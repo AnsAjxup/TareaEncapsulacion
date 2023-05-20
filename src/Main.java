42d1c6caf3e8fa33169d2e57e7f703dc45ccd1e4
AnsAjxup <aajxuph@miumg.edu.gt>
2023-05-19 23:39:32 -0600

    tarea terminada

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Persona persona = new Persona();
+        persona.setEdad(25);
+        persona.setNombre("Carlos");
+        persona.setTelefono(78965423);
+
+        System.out.println(persona.getEdad());
+        System.out.println(persona.getNombre());
+        System.out.println(persona.getTelefono());
     }
 }
 
@@ -8,4 +15,26 @@ class Persona{
     private int edad;
     private String nombre;
     private int telefono;
+
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+    public int getEdad(){
+        return this.edad;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+    public int getTelefono() {
+        return telefono;
+    }
 }
