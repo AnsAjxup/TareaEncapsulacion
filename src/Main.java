6559c811d75a832c836bda362219a0e328494146
AnsAjxup <aajxuph@miumg.edu.gt>
2023-05-19 23:26:46 -0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+class Persona{
+    private int edad;
+    private String nombre;
+    private int telefono;
+}
